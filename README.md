f33d3e0fbe5cd96ba3fa3a088e474851609f222f
Alexander Suter <alexander.suter@axonivy.com>
2023-12-25 16:16:09 +0100

    Small improvements

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # mythen-shooers.ch
 
 Plugin and Theme for mythen-shooters.ch
+
+## Deployment
+
+Execute `./deploy.sh`
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -r --verbose --update ./wp-theme-shooters/shooters/* suterale@hc-goldau.ch:/home/suterale/www/hopp.mythen-shooters.ch/wp-content/themes/shooters
