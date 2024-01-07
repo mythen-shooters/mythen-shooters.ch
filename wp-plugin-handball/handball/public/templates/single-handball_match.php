b6c053ba1b764cd3fd9ae976ed594285d2b65fc1
Alexander Suter <alexander.suter@axonivy.com>
2024-01-07 15:54:53 +0100

    Keep improving

--- wp-plugin-handball/handball/public/templates/single-handball_match.php
+++ wp-plugin-handball/handball/public/templates/single-handball_match.php
@@ -88,7 +88,7 @@ get_header(); ?>
   <?= $match->getVenueZip() ?>
   <?= $match->getVenueCity() ?>
   <iframe width="100%" height="600" style="margin-top:10px;height:300px;margin-bottom:10px;"
-    src="https://maps.google.com/maps?width=100%&amp;height=300&amp;hl=de&amp;q=<?= $halle ?>&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
+    src="https://maps.google.com/maps?width=100%&amp;height=300&amp;hl=de&amp;q=<?= $halle ?>&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed"
     frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
   </iframe>
 
